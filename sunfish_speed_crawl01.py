91866790a59b949b20f27e2ad5897ec253423c24
marinehayong <marinehayong@gmail.com>
2020-03-30 13:28:35 +0900

    네이트판 오늘의 새글 크롤링 해보기

--- sunfish_speed_crawl01.py
+++ sunfish_speed_crawl01.py
@@ -1,3 +1,13 @@
-print("hello python")
-print("hello python")
-print("hello python")
+# 네이트판 크롤링
+
+import requests
+from bs4 import BeautifulSoup
+
+# 엔터치기
+req = requests.get("https://pann.nate.com/talk/c20002?page=1")
+
+# 이런 식으로 HTML에 있는 코드를 다 가져온다.
+soup = BeautifulSoup(req.text, 'html.parser')
+
+for i in soup.select("#searchDiv > div.posting_wrap > table > tbody > tr"):
+    print(i.find("a").text)
