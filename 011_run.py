c1e4dcaf28493de2ce311e2493dcac3d58db610f
marinehayong <marinehayong@gmail.com>
2020-05-04 08:03:08 +0900

    오우 신기해 오우 재밌어 일단 프로그래밍부터 하고 공부하기 전법

--- 011_run.py
+++ 011_run.py
@@ -1,15 +1,22 @@
 from flask import Flask
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "<h5>헬로 파이썬</h5>"
+@app.route("/write", methods=["GET", "POST"])
+def board_write():
+    if request.method == "POST":
+        name = request.form.get("name")
+        title = request.form.get("title")        
+        contents = request.form.get("contents")
+    else:
+        return render_template("write.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
 
 # 직접 실행이 되었느냐, 아니면 모듈에서 임포트 되어 실행이 되었느냐
-# 직접실행 했기 때문에 이 구간이 실행됩니다
+# 직접실행 했기 때문에 이 구간이 실행.
