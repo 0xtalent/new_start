be6b9061e810866075ec359d23324441f4a82b57
marinehayong <marinehayong@gmail.com>
2020-05-04 08:58:35 +0900

    robo 3t에서 확인할 수 있군요

--- 011_run.py
+++ 011_run.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myweb"
+mongo = PyMongo(app)
 
 
 @app.route("/write", methods=["GET", "POST"])
@@ -12,13 +15,23 @@ def board_write():
         title = request.form.get("title")        
         contents = request.form.get("contents")
         print(name, title, contents)
+
+        board = mongo.db.board
+        post = {
+            "name": name,
+            "title": title,
+            "contents": contents,
+        }
+
+        board.insert_one(post)
+
         return ""
     else:
         return render_template("write.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True, port=9000)
 
 # 직접 실행이 되었느냐, 아니면 모듈에서 임포트 되어 실행이 되었느냐
 # 직접실행 했기 때문에 이 구간이 실행.
