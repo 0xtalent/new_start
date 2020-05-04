80b267c5168ddf9c47129978680e4b653015e2ed
marinehayong <marinehayong@gmail.com>
2020-05-04 09:06:28 +0900

    정말 재밌구만 datetime까지 구현완료

--- 011_run.py
+++ 011_run.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask_pymongo import PyMongo
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myweb"
@@ -16,16 +17,19 @@ def board_write():
         contents = request.form.get("contents")
         print(name, title, contents)
 
+        current_utc_time = round(datetime.utcnow().timestamp() * 1000)
         board = mongo.db.board
         post = {
             "name": name,
             "title": title,
             "contents": contents,
+            "pubdate": current_utc_time,
+            "view": 0,
         }
 
-        board.insert_one(post)
-
-        return ""
+        x = board.insert_one(post)
+        print(x.inserted_id)
+        return str(x.inserted_id)
     else:
         return render_template("write.html")
 
